892c9e31e7810cc20da8bdf03fd979fd61113cb0
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-12-16 17:07:58 +0700

    create class queues

--- Program.cs
+++ Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace sample_circle_queues
 {
-    internal class Program
+    class Queues
     {
+
+        int FRONT, REAR, max = 5;
+        int[]queue_array= new int[5];
+        public Queues()
+        {
+            //initizlizing the values of the variable REAR and FRONT to -1
+            //yo indicate that the queue is initially empty
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         static void Main(string[] args)
         {
         }
-    }
+    
 }
