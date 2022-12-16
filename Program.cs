9ea6bbf24049d77c9312ed2d3d1e9d562f66c690
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-12-16 17:13:05 +0700

    add public void insert

--- Program.cs
+++ Program.cs
@@ -18,7 +18,26 @@ namespace sample_circle_queues
             FRONT = -1;
             REAR = -1;
         }
-    }
+        public void insert(int element)
+        {
+            /* The following statement checks whether the queue is empty. If the queue is empty, then the value of the REAR and FRONT variable is set to 0. */
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                /* If REAR is at the last position of the array, then the value of REAR is set to 0 that corresponds to the first position of the array. */
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    /*If REAR is not at the last position, then its value is incremend by one.  */
+                    REAR = REAR + 1;
+            }
+            /* Once the position of REAR is determined, the element is added at its proper place. */
+            queue_array[REAR] = element;
+        }
 
 
 
